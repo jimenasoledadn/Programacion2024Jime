33d3e5ea0f2f80171efa47e02683e52cf48ff785
Jimena Nuñez <86858985+jimenasoledadn@users.noreply.github.com>
2024-04-16 22:29:17 -0300

    Add files via upload

---
+++ Clase 2/EjercNivelacion.py
@@ -0,0 +1,33 @@
+#   Ejercicio Nivelación
+# Una reconocida productora de contenidos audiovisuales está en busca de nuevas ideas
+# para su próximo proyecto, que promete cautivar al público.
+# Las posibles temáticas para explorar son las siguientes:
+#   • Comedia
+#   • Ciencia ficción
+#   • Drama
+# Para ello, la empresa ha decidido realizar una encuesta entre sus empleados para
+# recopilar información valiosa. Los datos para recopilar por cada empleado son los
+# siguientes:
+#   A) Información a ingresar por cada empleado encuestado:
+#       • Nombre del empleado
+#       • Edad (debe ser mayor o igual a 18)
+#       • Género (Masculino - Femenino)
+#       • Temática de interés (Comedia, Ciencia ficción, Drama)
+#   B) Se deben cargar 10 encuestas a través de la terminal.
+#   C) Se requiere determinar:
+#       • La cantidad de empleados de género masculino que votaron por Ciencia ficción o
+#           Drama, cuya edad esté entre 25 y 50 años, inclusive.
+#       • El porcentaje de empleados que no votaron por Comedia, siempre y cuando su
+#           género no sea Femenino o su edad esté comprendida entre 30 y 40 años.
+#       • El nombre y la temática de interés votada por el empleado masculino de mayor
+#           edad.
+
+CANT_ ENCUENTAS = 10
+
+for i in range(2):
+    nombre = input("ingrese nombre: ")
+
+
+
+
+
